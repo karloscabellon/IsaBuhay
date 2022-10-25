4416be2a7940c4c4ba8d62c5b780437fc8d9225c
Marc John <corralmarcjohno.com>
2022-10-25 12:52:21 +0800

    added code

--- IsabuhayWebsite/settings.py
+++ IsabuhayWebsite/settings.py
@@ -134,3 +134,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY='same-origin-allow-popups'
