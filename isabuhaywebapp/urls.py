202ca29a27d40724a61787782f29d6309b784c27
JEFFERSON ABELLANOSA <JEFFERSON.ABELLANOSA@cit.edu>
2022-10-25 10:48:22 +0800

    authentication now works, added logout

--- isabuhaywebapp/urls.py
+++ isabuhaywebapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('clientSide/', DisplayClientSide.as_view(), name='DisplayClientSide'),
     path('register/', CreateAccountPage.as_view(), name='CreateAccountPage'),
     path('login/', DisplayLoginPage.as_view(), name='DisplayLoginPage'),
+    path('logout/', LogoutView.as_view(), name="LogoutView"),
     path('account/', DisplayAccountPage.as_view(), name='DisplayAccountPage'),
     path('account/update', UpdateAccountPage.as_view(), name='UpdateAccountPage'),
     path('account/delete', DeleteAccountPage.as_view(), name='DeleteAccountPage'),
