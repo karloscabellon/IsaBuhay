e3114de3de4fa0f1c327227cd970a8f650c3355e
Marc John <corralmarcjohno.com>
2022-10-29 11:48:38 +0800

    new changes

--- static/js/index.js
+++ static/js/index.js
@@ -9,11 +9,25 @@
   photo = document.getElementById('photo');
   startbutton = document.getElementById('startbutton');
 
+  const constraints = {
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440,
+      },
+    },
+  }
+
   async function startWebcam(){
     try {
-      navigator.mediaDevices.getUserMedia({video: {width: {min: 1024, ideal: 1280, max:1920}, height: {min: 576, ideal: 720, max: 1080}}, audio: false});
-      video.srcObject = stream;
-      window.stream = stream;
+      const videoStream = await navigator.mediaDevices.getUserMedia(constraints)
+      video.srcObject = videoStream;
     } catch (e) {
       console.log("An error occurred: " + e.toString());
       
