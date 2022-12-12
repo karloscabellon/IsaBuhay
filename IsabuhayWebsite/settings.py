9ffedf08493ecccd73357204cc749faf19496a0e
Marc John <corralmarcjohno.com>
2022-12-12 08:48:20 +0800

    changes

--- IsabuhayWebsite/settings.py
+++ IsabuhayWebsite/settings.py
@@ -131,7 +131,6 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/media')
 
 
 # Default primary key field type
