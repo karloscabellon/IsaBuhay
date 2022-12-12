8ef0c1918d87e1ccf4891f516d5ad5c281a14255
Marc John <corralmarcjohno.com>
2022-12-12 08:17:42 +0800

    changes

--- IsabuhayWebsite/settings.py
+++ IsabuhayWebsite/settings.py
@@ -129,9 +129,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+# STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
